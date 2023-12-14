8dea116d84baa02a8c5c99e0a1a9ae9e9380e122
williamjpriest <liuliu6wp@gmail.com>
2023-12-14 01:30:52 -0500

    created-booking

--- src/app/connect.js
+++ src/app/connect.js
@@ -1,10 +1,10 @@
 const sqlite3 = require("sqlite3").verbose();
-export {}
+
 
 const db = new sqlite3.Database(
   "./collection.db",
   sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
-  (err:any) => {
+  (err) => {
     if (err) {
       return console.error(err.message);
     }
@@ -19,12 +19,13 @@ db.serialize(() => {
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         date TEXT,
         tickets TEXT,
-        no.tickets int
+        notickets int,
         price int
       )`,
-    (err:any) => {
+    (err) => {
       if (err) {
         return console.error(err.message);
       }
       console.log("Created items table.");
     })})
+    
